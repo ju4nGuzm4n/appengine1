ad9b403f178c11d81085f167db7b56831bb93119
juan <ivan.valencia@proveedoresgnp.mx>
2023-03-07 19:46:24 -0600

    cambios para hacer funcionar la app

--- src/main/java/com/example/demo/config/CORSConfig.java
+++ src/main/java/com/example/demo/config/CORSConfig.java
@@ -1,6 +1,5 @@
 package com.example.demo.config;
 
-package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
